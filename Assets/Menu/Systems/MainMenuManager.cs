cbb39d4a093971edecd070673e8b050bc4847c01
Asce <vchuynhchihieu@gmail.com>
2025-04-19 20:28:19 +0700

    Create Main Menu

---
+++ Assets/Menu/Systems/MainMenuManager.cs
@@ -0,0 +1,39 @@
+using Asce.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : Singleton<MainMenuManager>
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _settingsButton;
+    [SerializeField] private Button _quitButton;
+
+
+    public Button PlayButton => _playButton;
+    public Button SettingsButton => _settingsButton;
+    public Button QuitButton => _quitButton;
+
+
+    private void Start()
+    {
+        if (PlayButton != null) PlayButton.onClick.AddListener(PlayButton_OnClick);
+        if (SettingsButton != null) SettingsButton.onClick.AddListener(SettingsButton_OnClick);
+        if (QuitButton != null) QuitButton.onClick.AddListener(QuitButton_OnClick);
+
+    }
+
+    private void PlayButton_OnClick()
+    {
+        SceneLoader.Instance.LoadScene("Game");
+    }
+
+    private void SettingsButton_OnClick()
+    {
+
+    }
+
+    private void QuitButton_OnClick()
+    {
+        Application.Quit();
+    }
+}
